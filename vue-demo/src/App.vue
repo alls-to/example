8da12073c2b4bd07cc99ce018a43a7bcbd168dc4
dream <dream>
2024-07-11 23:11:49 +0800

    feat: init

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,45 @@
+<!-- App.vue -->
+<template>
+  <div>
+    <PaymentButton :onCompleted="onCompleted" :option="option" :host="host" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import PaymentButton from '@alls-to/vue'
+
+
+
+export default defineComponent({
+  name: 'App-Demo',
+  methods: {},
+  components: {
+    PaymentButton,
+  },
+
+  setup(props) {
+    function onCompleted(e: any) {
+      console.log('Payment completed!', e)
+    }
+
+    const option = {
+      customer: '',
+      id: 'allsto-demo/product1',
+      method: 'crypto,fiat',
+    }
+    // const host = `https://gateway.alls.to/checkout`
+     const host = `http://localhost:3000/checkout`
+
+    return { onCompleted, option, host }
+  },
+
+})
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
